e9c37630039ecb8c94a011e2e2ce0e5d809c1dd7
AR123456 <coloradoz99@gmail.com>
2019-07-21 06:05:36 -0600

    adjusting time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,7 +29,7 @@ function updateBackground() {
     bstyle = body.style,
     // hello = document.querySelector(".hello");
     bed = document.querySelector(".bed");
-  if (hr < 7) {
+  if (hr < 6) {
     bstyle.backgroundColor = "pink";
     bstyle.color = "black";
     // hello.innerText = "Not time yet";
