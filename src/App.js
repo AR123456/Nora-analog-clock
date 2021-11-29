38896e32591ab119666a1fd7241ad44f094ce144
AR123456 <coloradoz99@gmail.com>
2021-11-29 06:33:13 -0700

    expens custom component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visable! </p>
-      {/* If this was just JS would look like this :
-      const para = document.createElement("p");
-      para.textContent = "This is also visable"
-      document.getElementById("root").append(para)
-      
-      */}
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,6 @@
+// a component is just a JS function !!
+function ExpenseItem() {
+  return <h2>This is an expense item</h2>;
+}
+
+export default ExpenseItem;
