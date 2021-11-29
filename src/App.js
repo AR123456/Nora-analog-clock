93d065202cc41ca8405aa480b47b3b3a1748544c
AR123456 <coloradoz99@gmail.com>
2021-11-29 06:32:42 -0700

    how react works

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visable! </p>
+      {/* If this was just JS would look like this :
+      const para = document.createElement("p");
+      para.textContent = "This is also visable"
+      document.getElementById("root").append(para)
+      
+      */}
     </div>
   );
 }
