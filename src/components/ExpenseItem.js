a3898d8c737e51911c8065d4f1b64754f0569c3e
AR123456 <coloradoz99@gmail.com>
2021-12-01 06:19:08 -0700

    add css to custom component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+// inject the css expenseitem css
+import "./ExpenseItem.css";
 // a component is just a JS function !!
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>October 12, 2021</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   );
