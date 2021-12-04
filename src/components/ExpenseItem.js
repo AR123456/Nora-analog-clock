803f14796f9a303b5eba468fdef30192985fd6d7
AR123456 <coloradoz99@gmail.com>
2021-12-04 07:01:39 -0700

    children props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,7 @@
 // inject the css expenseitem css
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-
+import Card from "./Card";
 function ExpenseItem(props) {
   // can put some JS here - destructureing a bit to make the components more clear
   // const month = props.date.toLocaleString("en-US", { month: "long" });
@@ -9,13 +9,13 @@ function ExpenseItem(props) {
   // const year = props.date.getFullYear();
   console.log(props);
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
