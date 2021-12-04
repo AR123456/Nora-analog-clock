40318a8545fd6c6e28cf4bca106ae4125651a38d
AR123456 <coloradoz99@gmail.com>
2021-12-04 07:02:12 -0700

    using es6

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,23 @@
+// inject the css expenseitem css
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  // can put some JS here - destructureing a bit to make the components more clear
+  // const month = props.date.toLocaleString("en-US", { month: "long" });
+  // const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  // const year = props.date.getFullYear();
+  console.log(props);
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
