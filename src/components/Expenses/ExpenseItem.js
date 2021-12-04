deb6f12f5413a9f383cb7b09f4c2b3f46b630fcc
AR123456 <coloradoz99@gmail.com>
2021-12-04 07:03:29 -0700

    events and event handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,11 +4,9 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  // can put some JS here - destructureing a bit to make the components more clear
-  // const month = props.date.toLocaleString("en-US", { month: "long" });
-  // const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  // const year = props.date.getFullYear();
-  console.log(props);
+  const clickHandler = () => {
+    console.log("Clicked ");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
@@ -16,6 +14,16 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button
+        // can be in line function like this or a function we have defined elseware
+        // onClick={() => {
+        //   console.log("clicked");
+        // }}
+        // not executing here clickHandler()  just pointing to it, no parens.
+        onClick={clickHandler}
+      >
+        Change title
+      </button>
     </Card>
   );
 };
