ca837937c4048a4141af039c814ecc42ec1ffb70
AR123456 <coloradoz99@gmail.com>
2021-12-05 15:02:45 -0700

    adding from inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,16 +6,10 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  // useState needs a default action and updating funciton - this is an array
-  // can use array destructuring for this
   const [title, setTitle] = useState(props.title);
-  // State can be updated in many ways!
-  //  Http requests that complete (where we then want to update the state based on the Http response we got back) but you could also be updating state because a timer (set with setTimeout()) expired for example.
 
   const clickHandler = () => {
-    // call the state updating function
     setTitle("Updated");
-    console.log(title);
   };
   return (
     <Card className="expense-item">
