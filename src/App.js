8623a0df81c787810dcc0dade0c952682bdeaf1f
AR123456 <coloradoz99@gmail.com>
2021-12-07 06:52:37 -0700

    dynamic stateful lists

--- src/App.js
+++ src/App.js
@@ -1,47 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
-
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
+  // move this in to a global
+  // const expenses = [
+  //   {
+  //     id: "e1",
+  //     title: "Toilet Paper",
+  //     amount: 94.12,
+  //     date: new Date(2020, 7, 14),
+  //   },
+  //   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  //   {
+  //     id: "e3",
+  //     title: "Car Insurance",
+  //     amount: 294.67,
+  //     date: new Date(2021, 2, 28),
+  //   },
+  //   {
+  //     id: "e4",
+  //     title: "New Desk (Wooden)",
+  //     amount: 450,
+  //     date: new Date(2021, 5, 12),
+  //   },
+  // ];
+  // const [getter, setter] = useState(DUMMY_EXPENSES);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    // console.log("In App.js");
+    // console.log(expense);
+    // need to get this into expenses array but cannot just add becuase react would not update.
+    // so need to use state to do this - pass in a pointer function  as an arg that will recive the previous state snapshot
+    // use spread operator to create copy
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
-
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      {/* the items prop points at this array [expenses] */}
+
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,17 +13,13 @@ const Expenses = (props) => {
   };
 
   return (
-    // render dynamicaly first pass as prop from app [exenses] js to here
-    // one expense item pre item in the expense array
     <div>
       <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {/* execute this JS create new array from the former array */}
 
-        {/* [ExpenseItem,ExpenseItem,ExpenseItem]  trans for the expense item object into an array of JSX*/}
         {props.items.map((expense) => (
           <ExpenseItem
             title={expense.title}
@@ -31,26 +27,6 @@ const Expenses = (props) => {
             date={expense.date}
           />
         ))}
-        {/* <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        /> */}
       </Card>
     </div>
   );
