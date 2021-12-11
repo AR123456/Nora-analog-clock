2f2b73bd5f5d4159a4a4c3812571d196927e0ce7
AR123456 <coloradoz99@gmail.com>
2021-12-11 06:12:51 -0700

    fetch movies from the star wars api starter

--- src/App.js
+++ src/App.js
@@ -1,71 +1,34 @@
-import React, { useState } from "react";
+import React from 'react';
 
-import NewExpense from "./components/NewExpense/NewExpense";
-import Expenses from "./components/Expenses/Expenses";
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
-const App = () => {
-  // move this in to a global
-  // const expenses = [
-  //   {
-  //     id: "e1",
-  //     title: "Toilet Paper",
-  //     amount: 94.12,
-  //     date: new Date(2020, 7, 14),
-  //   },
-  //   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  //   {
-  //     id: "e3",
-  //     title: "Car Insurance",
-  //     amount: 294.67,
-  //     date: new Date(2021, 2, 28),
-  //   },
-  //   {
-  //     id: "e4",
-  //     title: "New Desk (Wooden)",
-  //     amount: 450,
-  //     date: new Date(2021, 5, 12),
-  //   },
-  // ];
-  // const [getter, setter] = useState(DUMMY_EXPENSES);
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-  const addExpenseHandler = (expense) => {
-    // console.log("In App.js");
-    // console.log(expense);
-    // need to get this into expenses array but cannot just add becuase react would not update.
-    // so need to use state to do this - pass in a pointer function  as an arg that will recive the previous state snapshot
-    // use spread operator to create copy
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
-    });
-  };
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
 
   return (
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-
-      <Expenses items={expenses} />
-    </div>
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
-};
+}
 
 export default App;
