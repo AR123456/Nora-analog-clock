4f2dc57c7651af155147b32f076571a3f5ea041c
AR123456 <coloradoz99@gmail.com>
2021-12-11 04:57:31 -0700

    expense chart working

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,23 +1,18 @@
 import React from "react";
+
 import ChartBar from "./ChartBar";
 import "./Chart.css";
+
 const Chart = (props) => {
-  // going to need this max in the ExpensesChart.js component
-  // map the objects into numbers
   const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
-  // dataPointValues is an array of numbers now so need to use the spread
-  // operator and pull them out as stand alone arguments
   const totalMaximum = Math.max(...dataPointValues);
 
-  // recive data as props to make chartBars
   return (
     <div className="chart">
-      <div>Chart goes here</div>
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          // pass in cacluated totalMax value
           maxValue={totalMaximum}
           label={dataPoint.label}
         />
--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Chart from "../Chart/Chart.css";
+import Chart from "../Chart/Chart";
 
 const ExpensesChart = (props) => {
   const chartDataPoints = [
